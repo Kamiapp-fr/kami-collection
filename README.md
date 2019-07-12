cab029882f1819a88b83393b6d9179cd79b61d29
Emilien <admin@emilienleroy.fr>
2019-07-13 00:09:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# `kami-collection`
+
+## Contribute
+
+We would love you for the contribution to ``kami-collection`` project, check the ``CONTRIBUTING`` file for more info.
+
---
+++ test/KamiCollection.test.ts
@@ -0,0 +1,5 @@
+
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
+import '@webcomponents/webcomponentsjs/webcomponents-bundle'
+
+describe('Component test', () => {})
