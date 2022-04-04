e457e649091b44890a55c6fbbc48df1b7fc83dbd
Emilien Leroy <emilien1005@hotmail.fr>
2022-04-04 21:05:18 +0000

    ✨ (markdown): Parse markdown directly from the markdown component

--- packages/markdown/src/kami-markdown.ts
+++ packages/markdown/src/kami-markdown.ts
@@ -1,11 +1,27 @@
 import { html, LitElement } from 'lit';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import Markdown from 'markdown-it';
 
 export default class KamiMarkdown extends LitElement {
   static get tag() {
     return 'kami-markdown';
   }
 
+  private content: string;
+
+  private parser: Markdown;
+
+  constructor() {
+    super();
+    this.parser = new Markdown();
+    this.content = this.innerHTML.trim().split('\n').reduce(this.reduceHtml, '');
+  }
+
+  private reduceHtml(content: string, text: string) {
+    return `${content}${text.trim()} \n`;
+  }
+
   protected render() {
-    return html`${KamiMarkdown.tag}`;
+    return html`${unsafeHTML(this.parser.render(this.content))}`;
   }
 }
