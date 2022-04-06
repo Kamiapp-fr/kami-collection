7dbec9d8db1f60a51b7309c3bb9906ce5866bae8
Emilien Leroy <emilien1005@hotmail.fr>
2022-04-06 21:47:39 +0000

    🎨 (markdown): Using slot instead of innerHtml
    Now the component get the content from the slot and watch it using slotchange listener

--- packages/markdown/src/kami-markdown.ts
+++ packages/markdown/src/kami-markdown.ts
@@ -1,4 +1,5 @@
 import { html, LitElement } from 'lit';
+import { state } from 'lit/decorators.js';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import Markdown from 'markdown-it';
 
@@ -7,31 +8,50 @@ export default class KamiMarkdown extends LitElement {
     return 'kami-markdown';
   }
 
+  @state()
   private content: string;
 
   private parser: Markdown;
 
   constructor() {
     super();
-    this.parser = new Markdown();
     this.content = '';
-  }
-
-  connectedCallback(): void {
-    super.connectedCallback();
-
-    this.content = this.innerHTML
-      .trim()
-      .replace(/<!--.*?-->/g, '')
-      .split('\n')
-      .reduce(this.reduceHtml, '');
+    this.parser = new Markdown();
   }
 
   private reduceHtml(content: string, text: string) {
     return `${content}${text.trim()} \n`;
   }
 
+  private handleSlotchange() {
+    const slot = this.shadowRoot?.querySelector('slot');
+
+    if (!slot) {
+      return;
+    }
+
+    // Get childrens nodes into the slot
+    // and get only Text node with some content.
+    // This prevent all trouble with script element.
+    const nodes = slot.assignedNodes({ flatten: true });
+    const texts = nodes
+      .map((node) => node instanceof Text && node.nodeValue)
+      .filter((x) => !!x) as string[];
+
+    this.content = texts
+      .reduce((a, b) => a + b, '')
+      .split('\n')
+      .reduce(this.reduceHtml, '');
+  }
+
   protected render() {
-    return html`${unsafeHTML(this.parser.render(this.content))}`;
+    return html`
+      <slot 
+        style="display: none" 
+        @slotchange=${this.handleSlotchange}
+      ></slot>
+
+      ${unsafeHTML(this.parser.render(this.content))}
+    `;
   }
 }
