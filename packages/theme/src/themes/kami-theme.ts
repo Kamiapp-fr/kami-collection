cec109b4ee25be7f2869fa2a0da1b23c4a6329c4
Emilien Leroy <emilien1005@hotmail.fr>
2022-04-13 11:39:13 +0200

    💄 (theme): Improve style of kami-theme

--- packages/theme/src/themes/kami-theme.ts
+++ packages/theme/src/themes/kami-theme.ts
@@ -9,21 +9,32 @@ export default class KamiTheme extends Theme {
 
   static styles = css`
     ::slotted(*) {
-      --kami-theme-font: "Gosha Sans";
+      --kami-theme-font-primary: "Gosha Sans";
+      --kami-theme-font-secondary: "UnB Office";
       --kami-theme-shadow: 0 0 8px -5px black;
+      --kami-theme-text-shadow: 0 0 4px #0000008a;
       --kami-theme-raduis: 20px;
+      --kami-theme-white: white;
     }
 
     .kami-light-mode::slotted(*) {
       --kami-theme-primary: #30E2C9;
-      --kami-theme-secondary: #8B8B8F;
-      --kami-theme-background: #F5F5FB;
+      --kami-theme-secondary: #5472ea;
+      --kami-theme-accent: #8B8B8F;
+      --kami-theme-text: #565656;
+      --kami-theme-background: white;
+      --kami-theme-gradient-primary: linear-gradient(116.95deg, #41B5FF -37.42%, #30E3CA 66.57%);
+      --kami-theme-gradient-secondary:  linear-gradient(116.95deg, #30E3CA 32.17%, #41B5FF 116.16%);
     }
   
     .kami-dark-mode::slotted(*) {
       --kami-theme-primary: #30E2C9;
-      --kami-theme-secondary: #8B8B8F;
+      --kami-theme-secondary: #5472ea;
+      --kami-theme-accent: #8B8B8F;
+      --kami-theme-text: white;
       --kami-theme-background: #303032;
+      --kami-theme-gradient-primary: linear-gradient(116.95deg, #41B5FF -37.42%, #30E3CA 66.57%);
+      --kami-theme-gradient-secondary:  linear-gradient(116.95deg, #30E3CA 32.17%, #41B5FF 116.16%);
     }
   `;
 }
