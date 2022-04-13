ab3dd8124d32b306a6fa1925e8cca5dc9e0a31f7
Emilien Leroy <emilien1005@hotmail.fr>
2022-04-13 08:40:55 +0200

    ✨ (theme): Store selected theme into local storage

--- packages/theme/src/themes/base-theme.ts
+++ packages/theme/src/themes/base-theme.ts
@@ -1,5 +1,5 @@
 import './fonts/index.css';
-import { html, LitElement } from 'lit';
+import { html, LitElement, PropertyValueMap } from 'lit';
 import { property } from 'lit/decorators.js';
 
 export default abstract class BaseTheme extends LitElement {
@@ -7,8 +7,40 @@ export default abstract class BaseTheme extends LitElement {
     throw new Error('Missing element tag');
   }
 
-  @property()
-  private readonly theme: 'light' | 'dark' = 'light';
+  @property({
+    reflect: true,
+  })
+  private theme: 'light' | 'dark' = 'light';
+
+  protected updated(_changedProperties: PropertyValueMap<any>): void {
+    const theme = _changedProperties.get('theme');
+
+    if (!theme) {
+      return;
+    }
+
+    this.updateTheme(this.theme);
+  }
+
+  public connectedCallback(): void {
+    super.connectedCallback();
+    const store = localStorage.getItem('kami-theme-mode');
+
+    if (!store) {
+      return;
+    }
+
+    this.updateTheme(store);
+  }
+
+  public updateTheme(theme: string) {
+    if (theme !== 'light' && theme !== 'dark') {
+      return;
+    }
+
+    this.theme = theme;
+    localStorage.setItem('kami-theme-mode', theme);
+  }
 
   protected render() {
     return html`
