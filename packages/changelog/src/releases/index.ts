afbe341b401382024e83100735a97cee1e0bd2b7
Emilien Leroy <emilien1005@hotmail.fr>
2022-04-14 10:35:52 +0200

    ✨ (changelog): Add gitlab provider

--- packages/changelog/src/releases/index.ts
+++ packages/changelog/src/releases/index.ts
@@ -1,11 +1,15 @@
 import ProviderRelease from './ProviderRelease';
 import GithubRelease from './GithubRelease';
+import GitlabRelease from './GitlabRelease';
 import Release from './Release';
 
 function releaseFactory(provider: ProviderRelease, data: any): Release {
   switch (provider) {
     case ProviderRelease.GITHUB:
       return new GithubRelease(data);
+
+    case ProviderRelease.GITLAB:
+      return new GitlabRelease(data);
 
     default:
       throw new TypeError('Provider doesn\'t exist or is not define !');
