fce5af07133ab0f5ceed64b4c2bbbf84f0062c5e
Emilien Leroy <emilien1005@hotmail.fr>
2022-04-21 15:41:46 +0200

    ✨ Update all root config file when you run create cmd

--- scripts/create.js
+++ scripts/create.js
@@ -6,10 +6,30 @@ module.exports = function (plop) {
             name: 'name',
             message: 'Name of your package ?'
         }],
-        actions: [{
-            type: 'addMany',
-            destination: '../packages/{{lowerCase name}}',
-            templateFiles: 'templates/**/*.hbs'
-        }]
+        actions: [
+            {
+                type: 'addMany',
+                destination: '../packages/{{lowerCase name}}',
+                templateFiles: 'templates/**/*.hbs'
+            },
+            {
+                type: 'append',
+                path: '../package.json',
+                pattern: /".\/packages\/component",/,
+                template: '    "./packages/{{lowerCase name}}",'
+            },
+            {
+                type: 'append',
+                path: '../vite.config.js',
+                pattern: /export const alias = {/,
+                template: '  "@kamiapp/{{lowerCase name}}": resolve(__dirname, "./packages/{{lowerCase name}}/src/index.ts"),'
+            },
+            {
+                type: 'append',
+                path: '../tsconfig.json',
+                pattern: /"paths": {/,
+                template: '      "@kamiapp/{{lowerCase name}}": ["./packages/{{lowerCase name}}/src/index.ts"],'
+            }
+        ]
     });
 };
