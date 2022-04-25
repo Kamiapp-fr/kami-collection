2a35fdc82a0cdfa83d3de3ec82a6e9611d648271
Emilien Leroy <emilien1005@hotmail.fr>
2022-04-25 21:55:09 +0000

    ✨ Add highlight.js and register js lang

--- demos/main.ts
+++ demos/main.ts
@@ -1,5 +1,7 @@
 import KamiTheme from '@kamiapp/theme';
 import KamiMarkdown from '@kamiapp/markdown';
+import javascript from 'highlight.js/lib/languages/javascript';
+import style from 'highlight.js/styles/gradient-dark.css';
 
 import './components/mode';
 import './components/button';
@@ -8,5 +10,7 @@ import './components/menu';
 import './components/home';
 import './components/guide';
 
+KamiMarkdown.registerHighlightLanguage('js', javascript);
+KamiMarkdown.registerHighlightStyle(style);
 window.customElements.define(KamiTheme.tag, KamiTheme);
 window.customElements.define(KamiMarkdown.tag, KamiMarkdown);
