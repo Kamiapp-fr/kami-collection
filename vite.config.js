0e45ff2b29fe27287e69e051f798ec21cbf115a6
Emilien Leroy <emilien1005@hotmail.fr>
2022-04-28 17:45:04 +0200

    ✨ Add and configure vite-plugin-cem

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,42 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path';
+import { resolve, dirname  } from 'path';
+import { fileURLToPath } from 'url';
+import VitePluginCustomElementsManifest from 'vite-plugin-cem';
+
+const _dirname = dirname(fileURLToPath(import.meta.url));
 
 export const alias = {
-  "@kamiapp/component": resolve(__dirname, "./packages/component/src/kami-component.ts"),
-  "@kamiapp/flash": resolve(__dirname, "./packages/flash/src/index.ts"),
-  "@kamiapp/infinite-list": resolve(__dirname, "./packages/infinite-list/src/index.ts"),
-  "@kamiapp/collection": resolve(__dirname, "./packages/collection/src/kami-collection.ts"),
-  "@kamiapp/markdown": resolve(__dirname, "./packages/markdown/src/kami-markdown.ts"),
-  "@kamiapp/changelog": resolve(__dirname, "./packages/changelog/src/kami-changelog.ts"),
-  "@kamiapp/theme": resolve(__dirname, "./packages/theme/src/index.ts"),
-  "@kamiapp/transition": resolve(__dirname, "./packages/transition/src/kami-transition.ts"),
+  "@kamiapp/component": resolve(_dirname, "./packages/component/src/kami-component.ts"),
+  "@kamiapp/flash": resolve(_dirname, "./packages/flash/src/index.ts"),
+  "@kamiapp/infinite-list": resolve(_dirname, "./packages/infinite-list/src/index.ts"),
+  "@kamiapp/collection": resolve(_dirname, "./packages/collection/src/kami-collection.ts"),
+  "@kamiapp/markdown": resolve(_dirname, "./packages/markdown/src/kami-markdown.ts"),
+  "@kamiapp/changelog": resolve(_dirname, "./packages/changelog/src/kami-changelog.ts"),
+  "@kamiapp/theme": resolve(_dirname, "./packages/theme/src/index.ts"),
+  "@kamiapp/transition": resolve(_dirname, "./packages/transition/src/kami-transition.ts"),
 }
 
 export default defineConfig({
   resolve: {
     alias,
   },
-  root: resolve(__dirname, './demos/'),
+  root: resolve(_dirname, './demos/'),
   build: {
-    outDir: resolve(__dirname, './dist/'),
+    outDir: resolve(_dirname, './dist/'),
     rollupOptions: {
       input: {
-        home: resolve(__dirname, './demos/index.html'),
-        started: resolve(__dirname, './demos/guide/index.html'),
+        home: resolve(_dirname, './demos/index.html'),
+        started: resolve(_dirname, './demos/guide/index.html'),
       }
     }
-  }
+  },
+  plugins: [
+    VitePluginCustomElementsManifest({
+      files: [
+        './demos/components/button.ts',
+        './packages/markdown/src/kami-markdown.ts'
+      ],
+      lit: true,
+    })
+  ]
 })
