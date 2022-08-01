4b98f18ca4fb197681090f197d7d36233d37929c
Emilien Leroy <emilien1005@hotmail.fr>
2022-08-01 14:12:09 +0200

    🚧 (infinite-list): Add search-bar element. Also add sort and filter methods

--- packages/infinite-list/src/index.ts
+++ packages/infinite-list/src/index.ts
@@ -1,8 +1,17 @@
+import '@kamiapp/search-bar';
 import { css, html, LitElement } from 'lit';
 import {
   customElement, property, query, state,
 } from 'lit/decorators.js';
 import Mustache, { OpeningAndClosingTags } from 'mustache';
+
+interface KamiInfiniteListQuery {
+  src: string,
+  limit?: number,
+  page?: number,
+  sort?: number | string,
+  search?: string,
+}
 
 /**
  * @summary Infinite list based on web component.
@@ -12,18 +21,40 @@ import Mustache, { OpeningAndClosingTags } from 'mustache';
 export default class KamiInfiniteList extends LitElement {
   static styles = css`
     .kami-infinite-list {
-      display: var(--kami-infinite-list-display, block);
       height: var(--kami-infinite-list-height, 100%);
+      display: flex;
+      flex-direction: column;
+    }
+
+    .kami-infinite-list__container {
+      display: var(--kami-infinite-list-display, block);
       flex-wrap: var(--kami-infinite-list-wrap, wrap);
       justify-content: var(--kami-infinite-list-justify, flex-start);
       align-items: var(--kami-infinite-list-align, center);
       gap: var(--kami-infinite-list-gap, 10px);
       overflow: auto; 
+      flex-grow: 1;
+    }
+
+    .kami-infinite-list__search {
+      margin-bottom: 10px;
     }
   `;
 
   private get delimiters() {
     return [this.openDelimiter, this.closeDelimiter] as OpeningAndClosingTags;
+  }
+
+  private get customSort() {
+    if (this.order === 1 && this.queryAsc) {
+      return this.queryAsc;
+    }
+
+    if (this.order === -1 && this.queryDesc) {
+      return this.queryDesc;
+    }
+
+    return this.order as number;
   }
 
   @property({ type: String })
@@ -35,11 +66,29 @@ export default class KamiInfiniteList extends LitElement {
   @property({ type: Number })
   public limit = 10;
 
+  @property({ type: String })
+  public search?: string;
+
+  @property({ type: Number, reflect: true })
+  public order = 0;
+
   @property({ type: String, attribute: 'query-limit' })
   public queryLimit = '_limit';
 
   @property({ type: String, attribute: 'query-page' })
   public queryPage = '_page';
+
+  @property({ type: String, attribute: 'query-search' })
+  public querySearch = '_search';
+
+  @property({ type: String, attribute: 'query-sort' })
+  public querySort = '_sort';
+
+  @property({ attribute: 'query-asc' })
+  public queryAsc?: string | number;
+
+  @property({ attribute: 'query-desc' })
+  public queryDesc?: string | number;
 
   @property({ type: String, attribute: 'open-delimiter' })
   public openDelimiter = '${';
@@ -49,6 +98,9 @@ export default class KamiInfiniteList extends LitElement {
 
   @property({ type: Number, attribute: 'loading-at' })
   public loadingAt = 0;
+
+  @property({ type: Boolean, attribute: 'use-search-bar' })
+  public useSearchBar = false;
 
   private template?: HTMLTemplateElement;
 
@@ -71,6 +123,24 @@ export default class KamiInfiniteList extends LitElement {
     await this.load();
   }
 
+  public async filter(value: string) {
+    this.search = value;
+    this.clear();
+    await this.load();
+  }
+
+  public async sort(sort: number) {
+    this.order = sort;
+    this.clear();
+    await this.load();
+  }
+
+  public clear() {
+    this.page = 1;
+    this.data = [];
+    this.container.innerHTML = '';
+  }
+
   private async load() {
     if (!this.src) {
       return;
@@ -84,10 +154,13 @@ export default class KamiInfiniteList extends LitElement {
         src: this.src,
         limit: this.limit,
         page: this.page,
+        search: this.search,
+        sort: this.customSort,
       });
 
       this.data.push(...data);
       data.forEach(this.appendItem.bind(this));
+
       this.dispatchEvent(new CustomEvent('loading-success'));
     } catch (error) {
       this.dispatchEvent(new CustomEvent('loading-error'));
@@ -96,13 +169,27 @@ export default class KamiInfiniteList extends LitElement {
     }
   }
 
-  private async get({ src, limit, page }: { src: string, limit?: number, page?: number }) {
-    const params = new URLSearchParams({
-      [this.queryLimit]: (limit || this.limit).toString(),
-      [this.queryPage]: (page || this.page).toString(),
-    });
+  private async get({
+    src,
+    limit,
+    page,
+    sort,
+    search,
+  }: KamiInfiniteListQuery) {
+    const url = new URL(src);
 
-    const response = await fetch(`${src}?${params}`);
+    url.searchParams.append(this.queryLimit, (limit || this.limit).toString());
+    url.searchParams.append(this.queryPage, (page || this.page).toString());
+
+    if (search) {
+      url.searchParams.append(this.querySearch, search);
+    }
+
+    if (sort) {
+      url.searchParams.append(this.querySort, sort.toString());
+    }
+
+    const response = await fetch(url.toString());
     const data = await response.json();
 
     return data;
@@ -167,10 +254,56 @@ export default class KamiInfiniteList extends LitElement {
     }));
   }
 
+  private async onSearch({ detail }: CustomEvent) {
+    const { search } = detail;
+
+    if (!search) {
+      return;
+    }
+
+    await this.filter(search);
+  }
+
+  private async onClearSearch() {
+    this.search = undefined;
+    this.clear();
+    await this.load();
+  }
+
+  private async onSort({ detail }: CustomEvent) {
+    const { sort } = detail;
+
+    if (!sort) {
+      return;
+    }
+
+    await this.sort(sort);
+  }
+
+  private renderSearchBar() {
+    if (!this.useSearchBar) {
+      return html``;
+    }
+
+    return html`
+      <kami-search-bar 
+        class="kami-infinite-list__search"
+        @search="${this.onSearch}" 
+        @sort="${this.onSort}" 
+        @clear="${this.onClearSearch}" 
+      ></kami-search-bar>
+    `;
+  }
+
   protected render() {
     return html`
-      <div id="container" @scroll="${this.onScroll}" class="kami-infinite-list">
-        
+      <div class="kami-infinite-list">
+        ${this.renderSearchBar()}  
+        <div 
+          id="container" 
+          class="kami-infinite-list__container" 
+          @scroll="${this.onScroll}" 
+        ></div>
       </div>
     `;
   }
