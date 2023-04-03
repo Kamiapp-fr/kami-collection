e4433784e00d86f265eac0a9581a4990d40e5b83
Emilien Leroy <emilien1005@hotmail.fr>
2023-04-03 11:50:45 +0200

    🐛 (media): Fix kami-media build due to kami-pdf component

--- packages/media/vite.config.js
+++ packages/media/vite.config.js
@@ -9,6 +9,12 @@ module.exports = defineConfig({
       name: 'KamiMedia',
       fileName: (format) => `kami-media.${format}.js`
     },
+    rollupOptions: {
+      external: [
+        'pdfjs-dist/build/pdf.min.js',
+        'pdfjs-dist/build/pdf.worker.min.js?url',
+      ],
+    },
   },
   plugins: [dts()]
 })
